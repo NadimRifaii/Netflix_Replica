b148753572974342255b7589152d9c671856e6fb
Alisafa60 <alisafa6100@gmail.com>
2023-11-21 01:41:34 +0200

    added see also images component in 3rd page

---
+++ src/components/description/description.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DescriptionText from '../descriptionText/descriptionText.component.jsx'
+import VideoOverImage from '../image/descriptionImage.component.jsx';
+import './description.styles.css';
+
+const Description = ({ reverseOrder, header, paragraph, tvImage, tvVideo, applyConditionalStyle}) => {
+  const containerClassName = reverseOrder ? 'flex-row-reverse' : 'flex-row';
+  return (
+    <div className={`${containerClassName} description-container`}>
+      <DescriptionText header={header} paragraph={paragraph} />
+      <VideoOverImage tvImage={tvImage} tvVideo={tvVideo} applyConditionalStyle={applyConditionalStyle} />
+    </div>
+  );
+};
---
+++ src/components/imageContainer/image-container.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ImageCard from '../ImageCard/image-card.component'
+import './images-container.styles.css'
+
+const ImagesContainer=({images}) => {
+    
+    return(
+        <div className="images-container">
+        {images.map(image=>{
+            return <ImageCard image={image.src} />
+        })}
+        </div>
+    )
+}
