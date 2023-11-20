f7d7b2b6d344c8f79a7d4bc6018071bb7a8473c7
Abdallah-Katbai <abdallah.katbai@gmail.com>
2023-11-21 00:38:51 +0200

    added the Footer 2 components

---
+++ src/components/Footer2Components/Footer2.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import Btn1 from "../btns-components/Btn1"
+import "./Footer2.css"
+
+export default function Footer2(){
+    return(
+        <footer>
+            <div className="questions">Questions? Contact us.</div>
+            <ul className="list">
+                <li>FAQ</li>
+                <li>Help Center</li>
+                <li>Account</li>
+                <li>Media Center</li>
+                <li>Investor Relations</li>
+                <li>Jobs</li>
+                <li>Ways to Watch</li>
+                <li>Terms of Use</li>
+                <li>Privacy</li>
+                <li>Cookie preferences</li>
+                <li>Corporate Information</li>
+                <li>Contact Us</li>
+                <li>Speed Test</li>
+                <li>Legal Notices</li>
+                <li>Only on Netflix</li>
+            </ul>
+            <Btn1/>
+            <div className="batata">Netflix Lebanon</div>
+        </footer>
+    )
+}
