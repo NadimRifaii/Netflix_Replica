4d21825fefdcc4d62e62b43e78b9d6b370f6dbeb
NadimRifaii <nadimrifaii3@gmail.com>
2023-11-21 00:36:07 +0200

    added the utils folder

---
+++ src/utiles/api.utiles.js
@@ -0,0 +1,22 @@
+const MOVIE_GENRES_API = `https://api.themoviedb.org/3/genre/movie/list?api_key=ad1a2a4cb80d72ec16fe712446738ea8`
+export async function getMovieGenres() {
+  return fetch(MOVIE_GENRES_API)
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      return data
+    }).catch(error => {
+      return error
+    })
+}
+
+const MOVIES_BY_GENRE_API = `https://api.themoviedb.org/3/discover/movie?api_key=ad1a2a4cb80d72ec16fe712446738ea8&with_genres=`
+export async function getMoviesByGenre(genre) {
+  try {
+    const movies = await (await fetch(`${MOVIES_BY_GENRE_API}${genre}`)).json()
+    return movies
+  } catch (error) {
+    console.log(error)
+  }
+}
