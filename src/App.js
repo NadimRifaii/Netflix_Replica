5d5d7d32a7fbe3cd5729c24d79f57749ade160da
NadimRifaii <nadimrifaii3@gmail.com>
2023-11-22 00:47:25 +0200

    removed kamal files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import MovieDetails from './components/MovieDetails/movieDetails.component';
-import Genre from './components/Genre/genre.component.jsx'
 function App() {
   return (
     <>
-      <MovieDetails />
-      {/* <Genre /> */}
     </>
   );
 }
