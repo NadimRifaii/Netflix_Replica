0ae36fc480c6d71cd44103b9a69dfcb7a3e28232
NadimRifaii <nadimrifaii3@gmail.com>
2023-11-22 00:43:54 +0200

    fetched the comming soon

--- src/components/ComingSoonContainer/coming-soon-container.component.jsx
+++ src/components/ComingSoonContainer/coming-soon-container.component.jsx
@@ -8,25 +8,24 @@ const ComingSoonContainer = () => {
     const [comingSoon, setComingSoon] = useState([]);
 
     useEffect(() => {
-        const fetchDataAndSetComingSoon= async () => {
+        const fetchDataAndSetComingSoon = async () => {
             const data = await getComingSoonMovies();
             setComingSoon(data);
         };
         fetchDataAndSetComingSoon();
-      }, []);
+    }, []);
 
 
-    
-    return(
 
+    return (
         <div className="coming-soon-container">
-            {   comingSoon.length>0 &&
-                comingSoon.map(inf => {
-                    return(
-                        <ComingSoonText key={inf.id} header={inf.original_title} paragraph={inf.overview} />
+            {comingSoon.length > 0 &&
+                comingSoon.map((coming, index) => {
+                    return (
+                        index < 6 && <ComingSoonText key={coming.id} header={coming.original_title} paragraph={coming.overview} />
                     )
                 })
-            }   
+            }
         </div>
     );
 };
