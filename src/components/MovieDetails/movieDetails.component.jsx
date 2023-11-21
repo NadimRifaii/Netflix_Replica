9a052666b7a07ffdd33a424958131e2e9c3875bd
NadimRifaii <nadimrifaii3@gmail.com>
2023-11-21 04:22:05 +0200

    moved the utils folder to src folder

--- src/components/MovieDetails/movieDetails.component.jsx
+++ src/components/MovieDetails/movieDetails.component.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./movieDetails.style.css";
-import { getMovieDetails } from "../utils/api.utils";
+import { getMovieDetails } from '../../utils/api.utils.js'
 
 
 const MovieDetails = (props) => {
@@ -28,29 +28,29 @@ const MovieDetails = (props) => {
     }
     const { original_title: title, release_date: date, runtime, genres, overview, backdrop_path: bgImage, casts } = movieDetails
     return (
-        Object.keys(movieDetails).length>0 && //this is conditional rendering
+        Object.keys(movieDetails).length > 0 && //this is conditional rendering
         <div className="movie-details-container" style={{
             backgroundImage: `url(https://image.tmdb.org/t/p/w1280${bgImage})`,
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat',
         }} >
             <div className="movie-details">
-            <div>
-                <span class="movie-title"> <h2>{title}</h2></span>
+                <div>
+                    <span class="movie-title"> <h2>{title}</h2></span>
+                </div>
+                <div>
+                    <span class="movie-year">{date.split('-')[0]} |  </span>
+                    <span class="movie-rating">18+</span>
+                    <span class="movie-duration"> | {convertMinutesToHoursAndMinutes(runtime)} |  </span>
+                    <span class="movie-genre">{genres[0].name}</span>
+                </div>
+                <div className="desc">
+                    <span class="movie-plot">{overview}.</span>
+                </div>
+                <div>
+                    <span class="movie-stars">Starring: {getFirstThreeCast(casts.cast)}</span>
+                </div>
             </div>
-            <div>
-                <span class="movie-year">{date.split('-')[0]} |  </span>
-                <span class="movie-rating">18+</span>
-                <span class="movie-duration"> | {convertMinutesToHoursAndMinutes(runtime)} |  </span>
-                <span class="movie-genre">{genres[0].name}</span>
-            </div>
-            <div className="desc">
-                <span class="movie-plot">{overview}.</span>
-            </div>
-            <div>
-                <span class="movie-stars">Starring: {getFirstThreeCast(casts.cast)}</span>
-            </div>
-        </div>
         </div >
     );
 }
