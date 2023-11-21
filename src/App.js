6e5821f65b5c251f3cf3c43f85a83797282ca02a
NadimRifaii <nadimrifaii3@gmail.com>
2023-11-22 01:06:17 +0200

    x

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import MovieDetails from './components/MovieDetails/movieDetails.component';
 function App() {
   return (
     <>
--- src/utils/api.utils.js
+++ src/utils/api.utils.js
@@ -31,27 +31,6 @@ export async function getMovieDetails(movieId) {
     .catch(error => {
       console.log(error)
     })
-} const MOVIE_GENRES_API = `https://api.themoviedb.org/3/genre/movie/list?api_key=ad1a2a4cb80d72ec16fe712446738ea8`
-export async function getMovieGenres() {
-  return fetch(MOVIE_GENRES_API)
-    .then((response) => {
-      return response.json()
-    })
-    .then((data) => {
-      return data
-    }).catch(error => {
-      return error
-    })
-}
-
-const MOVIES_BY_GENRE_API = `https://api.themoviedb.org/3/discover/movie?api_key=ad1a2a4cb80d72ec16fe712446738ea8&with_genres=`
-export async function getMoviesByGenre(genre) {
-  try {
-    const movies = await (await fetch(`${MOVIES_BY_GENRE_API}${genre}`)).json()
-    return movies
-  } catch (error) {
-    console.log(error)
-  }
 }
 
 
