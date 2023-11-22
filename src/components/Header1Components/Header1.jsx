e63340c83cd4e6b949a350643044c3a399349d0e
NadimRifaii <nadimrifaii3@gmail.com>
2023-11-22 21:15:56 +0200

    added link to logo

--- src/components/Header1Components/Header1.jsx
+++ src/components/Header1Components/Header1.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import './Header1Style.css'
 import { Outlet } from "react-router-dom"
+import { Link } from "react-router-dom"
 export default function Header1() {
     return (
         <>
             <div className="nav-1">
-                <img src='/netflix.png' alt="" className='netflix' />
-                <img src="/smallnetflix.png" alt="N" className="small-netflix"/>
+                <Link to={'/'} >
+                    <img src='/netflix.png' alt="" className='netflix' />
+                    <img src="/smallnetflix.png" alt="N" className="small-netflix" />
+                </Link>
                 <div className="nav-1-right">
                     <div className="unlimited white txt-13">UNLIMITED TV SHOWS AND MOVIES</div>
                     <button className="red-background white txt-13">JOIN NOW</button>
--- src/components/Header2Components/Header2.jsx
+++ src/components/Header2Components/Header2.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Btn2 from '../btns-components/Btn2'
 import './Header2Style.css'
-
+import { Link } from 'react-router-dom'
 export default function Header2() {
     return (
         <div className='nav-2'>
             <div className='nav-2-container'>
                 <div className='nav-2-innercontainer'>
-                    <img src="/netflix.png" alt="" className='netflix2' width='40px' />
+                    <Link to={'/'} >
+                        <img src="/netflix.png" alt="" className='netflix2' width='40px' />
+                    </Link>
                     <div className='nav-2-right'>
                         <Btn2 />
                         <button className='header2-signin'>SIGN IN</button>
